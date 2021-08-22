7b170a53f1f265e19f7ae47a8cfc0cb098f04409
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-22 11:51:14 +0300

    import create_app function and manager class to initialize the extension  and server clas s to help launch the server

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,13 @@ bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
 
-
+    #Creating the app configurations
     app.config.from_object(config_options(config_name))
 
-
+    #Initializing flask extensions
     bootstrap.init_app(app)
 
-
+    #Registering blueprint
     from.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
