67f55d7a17a96466feb0cb66c43eb0d916d3e707
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-22 11:36:29 +0300

    create a configure_request function that takes in the application instance to replace the values of the api-key and base-url None variables to application configurationobjects

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,14 @@
 import urllib.request,json
 from .models import Source,Article
 
+#Getting api key
+api_key = None
 
+#Getting the news source base url
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_SOURCE_API_KEY']
+    base_url = app.config['NEWS_SOURCE_BASE_URL']
 
