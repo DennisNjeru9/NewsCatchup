88e614dfa973bdcff711f0c7f103938fc9530225
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-25 09:55:57 +0300

    create test cases for source and article models

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template,request,redirect
+from flask.helpers import url_for
 from . import main
 from newsapi import NewsApiClient
 
@@ -31,8 +32,14 @@ def index():
 def get_results():
     keyword = request.form['keyword']
 
-    news = newsapi.get_top_headlines(q=keyword,category='general',language='en')
-    all_articles = newsapi.get_everything(q=keyword,language='en',page=1)
-    #print(news['articles'])
-    #print(all_articles['articles'])
-    return render_template('news.html',news=news['articles'], all_articles = all_articles['articles'])
+    #news=['newsapi.get_top_headlines(q=keyword,category="general",language="en")']
+    all_articles=['newsapi.get_everything(q=keyword,language="en",page=1)']
+    # print(news['articles'])
+    # print(all_articles['articles'])
+    
+    search_article=request.args.get('keyword')
+
+    if search_article:
+        return redirect(url_for('search', article_name = search_article))
+    else:
+        return render_template('news.html',all_articles = all_articles('articles'))
