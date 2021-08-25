ef9d7dd4b4a16f4420e13900e4984979840cb9dc
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-25 15:33:24 +0300

    add custom error function and template to function

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+#from flask_bootstrap import Bootstrap
 from config import config_options
-from flask_bootstrap import Bootstrap
 
-bootstrap = Bootstrap()
+
+#bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__,template_folder='templates')
@@ -11,13 +12,13 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     #Initializing flask extensions
-    bootstrap.init_app(app)
+    #bootstrap.init_app(app)
 
     #Registering blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-
+    #Setting up configuration
     from .requests import configure_request
     configure_request(app)
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,57 +1,25 @@
-from flask import render_template,request,redirect
-from flask.helpers import url_for
+from flask import render_template,request,redirect,url_for
 from . import main
-from newsapi import NewsApiClient
-
-#Getting api key
-my_api_key = None
-
-#Getting the news source base url
-#base_url = None
-
-def configure_request(app):
-    global my_api_key
-    my_api_key = app.config['API_KEY']
+from ..requests import get_articles,get_sources
 
 
 #Views
-@main.route('/',methods=['GET'])
+@main.route('/')
 def index():
-    newsapi = NewsApiClient(api_key=my_api_key)
-    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
-
-    articles = topheadlines['articles']
-    news = []
-    desc = []
-    img = []
-
-    for i in range(len(articles)):
-        my_articles = articles[i]
-
-        news.append(my_articles['title'])
-        desc.append(my_articles['description'])
-        img.append(my_articles['urlToImage'])
-
-    my_list = zip(news,desc,img)
-
+    sources = get_sources()
     title = 'Home - Welcome to the best news catch-up online website'
 
-    return render_template('index.html',title=title, context=my_list)
+    return render_template('index.html',title=title, sources = sources)
 
 
-# @main.route('/news/',methods=['POST'])
-# def get_results():
-#     newsapi = NewsApiClient(api_key=my_api_key)
-#     keyword = request.form['keyword']
+@main.route('/news/<source_url>')
+def source(source_url):
+    pass
 
-#     news= newsapi.get_top_headlines(q=keyword,category="general",language="en")']
-#     all_articles=['newsapi.get_everything(q=keyword,language="en",page=1)']
-#     # print(news['articles'])
-#     # print(all_articles['articles'])
-    
-#     search_article=request.args.get('keyword')
 
-#     if search_article:
-#         return redirect(url_for('search', article_name = search_article))
-#     else:
-#         return render_template('news.html',all_articles = all_articles('articles'))
+
+@main.route('/news/<source_name>', methods=['POST'])
+def news(source_name):
+    articles = get_articles(source_name)
+    title = 'Here are the search results'
+    return render_template('news.html',title=title, articles=articles)
